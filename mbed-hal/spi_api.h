04d5b25fcdd77e4cd5b8c1a220c6ec2639f30d03
Marcus Chang <marcus.chang@arm.com>
2015-05-18 15:01:19 +0100

    Added parameter to spi_format for setting bit order.

--- mbed-hal/spi_api.h
+++ mbed-hal/spi_api.h
@@ -84,10 +84,11 @@ void spi_free(spi_t *obj);
  * Set the number of bits per frame, configure clock polarity and phase, shift order and master/slave mode
  * @param[in,out] obj   The SPI object to configure
  * @param[in]     bits  The number of bits per frame
+ * @param[in]     order Bit order. Zero for MSB (standard) or non-zero for LSB.
  * @param[in]     mode  The SPI mode (clock polarity, phase, and shift direction)
  * @param[in]     slave Zero for master mode or non-zero for slave mode
  */
-void spi_format(spi_t *obj, int bits, int mode, int slave);
+void spi_format(spi_t *obj, int bits, int order, int mode, int slave);
 
 /** Set the SPI baud rate
  *
