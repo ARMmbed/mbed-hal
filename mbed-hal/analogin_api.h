298397eadb32be53b322fbb965135e538aa33944
Martin Kojtal <martin.kojtal@arm.com>
2015-10-19 15:28:39 +0200

    analogin - API documentation

--- mbed-hal/analogin_api.h
+++ mbed-hal/analogin_api.h
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+ * Copyright (c) 2006-2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,10 +24,30 @@
 extern "C" {
 #endif
 
+/** Analogin hal structure. analogin_s is declared in the target's hal
+ */
 typedef struct analogin_s analogin_t;
 
-void     analogin_init    (analogin_t *obj, PinName pin);
-float    analogin_read    (analogin_t *obj);
+/** Initialize the analogin peripheral
+ *
+ * Configures the pin used by analogin.
+ * @param[out] obj The analogin object to initialize
+ * @param[in]  pin The analogin pin
+ */
+void analogin_init(analogin_t *obj, PinName pin);
+
+/** Read the input voltage, represented as a float in the range [0.0, 1.0]
+ *
+ * @param[in] obj The analogin object
+ * @return A floating value representing the current input voltage
+ */
+float analogin_read(analogin_t *obj);
+
+/** Read the value from analogin pin, represented as a unsigned 16bit value
+ *
+ * @param[in] obj The analogin object
+ * @return An unsigned 16bit value representing the current input voltage
+ */
 uint16_t analogin_read_u16(analogin_t *obj);
 
 #ifdef __cplusplus
