731d1652d7ea479a008e85e2acdedb8e7958ecc1
Martin Kojtal <martin.kojtal@arm.com>
2015-10-20 14:35:07 +0200

    gpio irq - doxygen doc

--- mbed-hal/gpio_irq_api.h
+++ mbed-hal/gpio_irq_api.h
@@ -24,20 +24,28 @@
 extern "C" {
 #endif
 
+/** gpio IRQ events
+ */
 typedef enum {
     IRQ_NONE,
     IRQ_RISE,
     IRQ_FALL
 } gpio_irq_event;
 
+/** gpio hal structure. gpio_irq_s is declared in the target's hal
+ */
 typedef struct gpio_irq_s gpio_irq_t;
 
 typedef void (*gpio_irq_handler)(uint32_t id, gpio_irq_event event);
 
-int  gpio_irq_init(gpio_irq_t *obj, PinName pin, gpio_irq_handler handler, uint32_t id);
+int gpio_irq_init(gpio_irq_t *obj, PinName pin, gpio_irq_handler handler, uint32_t id);
+
 void gpio_irq_free(gpio_irq_t *obj);
-void gpio_irq_set (gpio_irq_t *obj, gpio_irq_event event, uint32_t enable);
+
+void gpio_irq_set(gpio_irq_t *obj, gpio_irq_event event, uint32_t enable);
+
 void gpio_irq_enable(gpio_irq_t *obj);
+
 void gpio_irq_disable(gpio_irq_t *obj);
 
 #ifdef __cplusplus
